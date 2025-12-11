eca1d10a9636200d913e38361f8a6cf8ee94ea50
zysws <130185390+zysws@users.noreply.github.com>
2025-12-11 23:30:44 +0000

    added bootloader

---
+++ bootloader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 142823291 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/refs/heads/main/mm2.lua"))()
+else
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/main/universal-aimbot.lua"))() -- universal script
+end
