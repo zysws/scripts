979ebf567b15bc46d5a2c58e41924c92644a6748
zysws <130185390+zysws@users.noreply.github.com>
2025-12-21 18:11:13 +0000

    fixed weird bug
    weird bug where fisch has 2 place id's

--- bootloader.lua
+++ bootloader.lua
@@ -1,7 +1,10 @@
 if game.PlaceId == 142823291 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/refs/heads/main/mm2.lua"))()
-elseif game.PlaceId == 16732694052 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/refs/heads/main/fisch.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/main/mm2.lua"))()
+    warn("[Info] Running supported script")
+elseif game.PlaceId == 16732694052 or 131716211654599 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/main/fisch.lua"))()
+    warn("[Info] Running supported script")
 else
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/main/universal-aimbot.lua"))() -- universal script
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/main/universal-aimbot.lua"))()
+    warn("[Info] Running non-supported script")
 end
