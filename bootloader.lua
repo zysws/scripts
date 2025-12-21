413bca7ca9e06d4757320aa3da4443f2a0b7bce4
zysws <130185390+zysws@users.noreply.github.com>
2025-12-21 17:04:56 +0000

    added fisch support

--- bootloader.lua
+++ bootloader.lua
@@ -1,5 +1,7 @@
 if game.PlaceId == 142823291 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/refs/heads/main/mm2.lua"))()
+elseif game.PlaceId == 16732694052 then
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/refs/heads/main/fisch.lua"))()
 else
   loadstring(game:HttpGet("https://raw.githubusercontent.com/zysws/scripts/main/universal-aimbot.lua"))() -- universal script
 end
